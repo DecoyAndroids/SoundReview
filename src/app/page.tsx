2baf7557b2bdcaefec789358b91364fd95449429
unknown <Bicblin02@gmail.com>
2025-02-14 13:44:13 +0600

    [~]minor update for redeploying

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ export default function HomePage() {
   return (
     <main className="flex min-h-screen font-ubuntu flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <h1>head1</h1>
-      <h1>head2</h1>
-      <h1>head3</h1>
+      <h2>head2</h2>
+      <h3>head3</h3>
     </main>
   );
 }
