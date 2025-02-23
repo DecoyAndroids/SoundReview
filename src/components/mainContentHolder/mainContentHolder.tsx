35133a15a7eb888cc4803bf4c950c843af7e4543
unknown <Bicblin02@gmail.com>
2025-02-23 13:57:25 +0600

    [~] fixed some variables

--- src/components/mainContentHolder/mainContentHolder.tsx
+++ src/components/mainContentHolder/mainContentHolder.tsx
@@ -1,7 +1,7 @@
 import { NewsBlock } from "./newsBlock/newsBlock";
 import styles from "./mainContentHolder.module.scss"
 import { SearchInput } from "../ui/SearchInput/searchInput";
-import * as Data from "../../app/data/newsBlockData"
+import * as NewsBlockData from "../../app/data/newsBlockData"
 import { NewReleasesBlock } from "./newReleasesBlock/NewReleasesBlock";
 import * as NewReleasesData from '../.././app/data/NewReleasesBlockData'
 
@@ -9,7 +9,7 @@ export const MainContentHolder: React.FC = () => {
     return (
         <div className={`grow w-[100%] ${styles.MainContentHolder}`}>
         <SearchInput/>
-        <NewsBlock/>
+        <NewsBlock newsBlockData={NewsBlockData.newsBlockData}/>
         <NewReleasesBlock NewReleaseBlockData={NewReleasesData.Data}/>
         </div>
     );
