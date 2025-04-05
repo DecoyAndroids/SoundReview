b79dee2bfdb237559772f2966b264b62f9b7c5a7
Timur <danilov.tim@inbox.ru>
2025-04-05 22:08:25 +0600

    [~] some fix

---
+++ src/components/playlistContentHolder/PlaylistContenHolder.tsx
@@ -0,0 +1,7 @@
+export const PlaylistContentHolder: React.FC = () => {
+    return (
+        <div className={`grow w-[100%] bg-[rgb(var(--blackbrown))] rounded-[9px]`}>
+            LFASD:LFJASF
+        </div>
+    );
+  };
