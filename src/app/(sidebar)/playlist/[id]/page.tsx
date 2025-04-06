db23ff58c8e8b7a98e0c70ac7da8c2bea6b537a2
Timur <danilov.tim@inbox.ru>
2025-04-06 13:48:57 +0600

    [~] some fix playlist page

--- src/app/(sidebar)/playlist/[id]/page.tsx
+++ src/app/(sidebar)/playlist/[id]/page.tsx
@@ -1,15 +1,15 @@
+'use client'
+import { useParams } from "next/navigation";
 import { PlaylistContentHolder } from "~/components/playlistContentHolder/PlaylistContenHolder";
 
-type Props = {
-  params: {
-    id: string;
-  };
-};
 
-export default function AlbumPage({ params }: Props) {
+
+export default function AlbumPage() {
+  const {id} = useParams()
+  const playlistId = Array.isArray(id) ? id[0] : id ?? '';
   return (
     <div className="flex grow mr-[15px] ml-[15px] mt-[15px] mb-[15px] text-white bg-[rgb(var(--blackbrown))] rounded-[9px]">
-      <PlaylistContentHolder playlistId={parseInt(params.id)} />
+      <PlaylistContentHolder playlistId={parseInt(playlistId!)} />
     </div>
   );
 }
